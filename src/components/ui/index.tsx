9f2e75a5b85abe76ff82ce4cef2eefdc1cab184e
Wilson Matokanovic Jubior <wilsonmjuniorx@gmail.com>
2025-09-17 00:30:08 -0300

    feat: export ui components

---
+++ src/components/ui/index.tsx
@@ -0,0 +1,8 @@
+export * from './Avatar';
+export * from './Badge';
+export * from './Button';
+export * from './Card';
+export * from './Header';
+export * from './Input';
+export * from './Text';
+
