98a47bcaab9c5ecf52829b2cd0393780b80800d6
Wilson Matokanovic Jubior <wilsonmjuniorx@gmail.com>
2025-09-17 00:05:31 -0300

    feat: config tanstack query provider

---
+++ src/app/_layout.tsx
@@ -0,0 +1,35 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Stack } from 'expo-router';
+
+import 'react-native-reanimated';
+
+export const unstable_settings = {
+  initialRouteName: '(tabs)',
+};
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: (failureCount, error: any) => {
+        if (error.isRateLimit) return false;
+        return failureCount < 2;
+      },
+    },
+  },
+});
+
+export default function RootLayout() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RootLayoutNav />
+    </QueryClientProvider>
+  );
+}
+
+function RootLayoutNav() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(tabs)" />
+    </Stack>
+  );
+}
