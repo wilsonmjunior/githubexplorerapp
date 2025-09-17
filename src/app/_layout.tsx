8428eb582775add83aa8bd8cd6b01b9610dd69bc
Wilson Matokanovic Jubior <wilsonmjuniorx@gmail.com>
2025-09-17 00:06:48 -0300

    feat: load font

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,9 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 
 import 'react-native-reanimated';
 
@@ -24,6 +27,25 @@ const queryClient = new QueryClient({
 });
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf'),
+    ...FontAwesome.font,
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <QueryClientProvider client={queryClient}>
       <RootLayoutNav />
